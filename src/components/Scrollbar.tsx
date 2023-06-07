e1754818e3adcc4d7a5a51645627e511ceb0d304
sanjeev <60956846+sanjeev@users.noreply.github.com>
2023-06-07 17:53:49 +0530

    first commit

--- src/components/Scrollbar.tsx
+++ src/components/Scrollbar.tsx
@@ -14,12 +14,12 @@ const Scrollbar = ({
   ...otherProps
 }: ScrollbarProps) => {
   return (
-    <>
+
 
     <SimpleBar className={className} style={style} {...otherProps}>
-      {children}
+       <>{children} </>
     </SimpleBar>
-    </>
+
   );
 };
 
